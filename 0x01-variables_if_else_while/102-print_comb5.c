db8fe0fbacdc207fa3949d6e4c9fc916dc7d9731
Andresmelek <candres.isaza@gmail.com>
2019-09-21 00:23:31 -0500

    Trying

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - combination
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int a;
+int b;
+int c;
+int d;
+
+for (a = 48; a <= 57; a++)
+{
+for (b = 48; b <= 57; b++)
+{
+for (c = 48; c <= 57; c++)
+{
+for (d = 48; d <= 57; d++)
+{
+if (a <= c && b < d)
+{
+putchar(a);
+putchar(b);
+putchar(' ');
+putchar(c);
+putchar(d);
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
