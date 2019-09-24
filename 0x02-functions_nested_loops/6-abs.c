510bacfadf0893585dfc9391257e9495dca5e183
Andresmelek <candres.isaza@gmail.com>
2019-09-24 00:19:25 +0000

    First part

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _abs(int)
+{
+
+	int p;
+	int n;
+	
+	n = p * -1;
+
+	if (p >= 0)
+		printf("%d", p);
+	else
+		printf("%d", n)
+	return (0);
+}
