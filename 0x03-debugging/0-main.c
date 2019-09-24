c2fcec78a6bebe82856e6ef63c382b69bf961cdb
Andresmelek <candres.isaza@gmail.com>
2019-09-24 21:58:07 +0000

    Function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* positive_or_negative - gives the correct output when given a case of 0
+* @int: parameter to check
+*
+* Return: 0
+*/
+
+void positive_or_negative(int)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+	return (0);
+}
