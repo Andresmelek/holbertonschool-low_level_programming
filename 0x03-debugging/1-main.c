dd9a360fefb8f1aed45b73a57f44e9404410aa12
Andresmelek <candres.isaza@gmail.com>
2019-09-24 22:58:54 +0000

    Commenting the error

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,12 +10,11 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-/* The local variable is set to be 0 */
 	i = 0;
-/* The loops says to go while i is less than 0 */
-	while (i < 10)
-/* So i will always less than 0 */
-	putchar(i);
+
+	/* while (i < 10) */
+
+	/* putchar(i); */
 	}
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
