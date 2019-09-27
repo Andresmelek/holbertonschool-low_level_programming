4690b47a2abb30ab896d8998eed03a39c4348d21
Andresmelek <candres.isaza@gmail.com>
2019-09-26 23:19:41 -0500

    Fixing Prime numbers3.0 and F Betty

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-/*
- * mian - finds and prints the largest prime factor of 61285247514
- * Return: lowest prime factor
- */
+ /**
+  * mainn - finds and prints the largest prime factor of 61285247514
+  * Return: lowest prime factor
+  */
 
 int main(void)
 {
 	long x;
 	long y = 612852475143;
 
-	for (x = 2; x > x; x++)
+	for (x = 2; y > x; x++)
 	{
 		while (y % x == 0)
 		{
