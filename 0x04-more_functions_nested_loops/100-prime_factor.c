08129eb453b1ee64b703e4ba41d868611ea4fdcf
Andresmelek <candres.isaza@gmail.com>
2019-09-26 23:08:45 -0500

    Fixing Prime numbers

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -3,19 +3,19 @@
  * mian - finds and prints the largest prime factor of 61285247514
  * Return: lowest prime factor
  */
+
 int main(void)
 {
-  long  int x;
-  long y = 61285247514;
-  
+	long y;
+	long x = 612852475143;
 
-  for(x = 2; x < y; x++)
-    {
-      while (y % x == 0)
+	for (y = 2; x > y; y++)
 	{
-	  y = (y / x);
+		while (n % y == 0)
+		{
+		  (x = x / y);
+		}
 	}
-    }
-  printf("%ld\n", x);
-  return (0);
+	printf("%ld\x", y);
+	return (0);
 }
