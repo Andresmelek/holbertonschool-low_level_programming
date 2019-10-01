5f0b9f17f1c5ebc238da58954a88c0aef985c5c0
Andresmelek <candres.isaza@gmail.com>
2019-10-01 14:09:43 +0000

    Fixing task 5 reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,25 +5,28 @@
  */
 void rev_string(char *s)
 {
-  int i;
-  int j;
-  char [500];
-  int x;
+	int i;
+	int j;
+	int x;
 
-  i = 0;
-  x = 0;
-  
-  while (s[i] != 0)
-    {
-      i++
-    }
-  for (j =i-1; j >= 0; j--)
-    {
-      buff[x] = s[b];
-      x++;
-    }
-  for (j = 0; s[j] != 0; '\0'; j++)
-    {
-      s[j] = buff[j];
-    }
+	char buff[500];
+
+	i = 0;
+	x = 0;
+
+	while (s[i] != '\0')
+	{
+	i++;
+	}
+	{
+	for (j = i - 1; j >= 0; j--)
+	{
+		buff[x] = s[j];
+		x++;
+	}
+	}
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		s[j] = buff[j];
+	}
 }
