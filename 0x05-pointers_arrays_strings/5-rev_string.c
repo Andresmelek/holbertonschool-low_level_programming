cc23b464bce2f6045813a36641529edbd5370192
Andresmelek <candres.isaza@gmail.com>
2019-10-01 00:06:50 -0500

    Missing betty

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,14 +5,25 @@
  */
 void rev_string(char *s)
 {
+  int i;
+  int j;
+  char [500];
+  int x;
 
-	
-	for (i = 0; s[i] != '\0'; i++)
-	{
-	return (s[i]);
-	}
-	{
-		for (j = i;s[j] != '\0'; j--)
-	return (s[j]);
-	}
+  i = 0;
+  x = 0;
+  
+  while (s[i] != 0)
+    {
+      i++
+    }
+  for (j =i-1; j >= 0; j--)
+    {
+      buff[x] = s[b];
+      x++;
+    }
+  for (j = 0; s[j] != 0; '\0'; j++)
+    {
+      s[j] = buff[j];
+    }
 }
