0a18f747a5cf433798ed53dd9731cfdbb37d2d0f
Andresmelek <candres.isaza@gmail.com>
2019-10-01 23:34:58 -0500

    trying again

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,24 +1,25 @@
 #include "holberton.h"
-/**
- * _atoi - Write a function that convert a string to an integer.
- * @s: string
- * Return: number and 0 when the number is zero
- */
-int _atoi(char *s)
-{
-	int i = 0;
-	int sign = 1;
-	int num = 0;
 
-	while ((s[i] '\0') && (s[i] <= 48 || s[i] <= 57))
+/**
+ * _atoi - Function that converts a string to an integer
+ * @s: String
+ * Return: Integer value
+ */
+
+
+int _atoi(char *s)
+
+{
+	int x = 0, i = 0, numb = -1;
+
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
 	{
-		if (s[i] = '45';
-			sign = -1;
-			i++;
+		if (s[i] == '-')
+			numb *= -1;
+		i++;
 	}
-	{
-	while ((s[i] '\0') && (s[i] >= 48 || s[i] <= 57))
-		num = num * 10 + s[i++] - '0';
-		return (sign * num);
-	}
+	while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
+		x = (x * 10) - (s[i++] - '0');
+
+	return (x * numb);
 }
