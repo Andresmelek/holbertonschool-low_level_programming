8a45c45919fa19558bf5ff8dc077a1612b104785
Andresmelek <candres.isaza@gmail.com>
2019-10-03 22:46:46 -0500

    New readme

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,7 +10,7 @@ char *leet(char *s)
 	char word[] = "AEOTLaeotl";
 	char num[] = "4307143701";
 
-	for (i = 0; s[i] != 0; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j <= 10; j++)
 		{
