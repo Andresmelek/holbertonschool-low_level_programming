ed0fc45f150cc703d70cd35669064fd7a5b632b0
Andresmelek <55111141+Andresmelek@users.noreply.github.com>
2019-10-03 22:31:33 -0500

    Add all files again

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * *cap_string - function that capitalizes all words of a string
+ * s*: string
+ * Return: returns the string
+ */
+char *cap_string(char *)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	if (s[i] == ' ' || s[i] == ';' || s[i] == '.'
+	|| s[i] == '!' || s[i] == ';' || s[i] == '?' || s[i] == '"'
+	|| s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
+
+}
