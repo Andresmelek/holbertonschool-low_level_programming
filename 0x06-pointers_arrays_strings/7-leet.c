408dc3e822154a9bac99c0acc84a26fe284aaac8
Andresmelek <candres.isaza@gmail.com>
2019-10-03 22:53:05 -0500

    Fixing 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,24 @@
 #include "holberton.h"
 /**
- * *leet - Write a function that encodes a string into 1337
- * @s: string
- * Return: String
+ * leet - function that encodes a string into 1337
+ * @s:  string
+ * Return: string encoded
  */
 char *leet(char *s)
 {
-	int i, j;
-	char word[] = "AEOTLaeotl";
-	char num[] = "4307143701";
-
+int i;
+int j;
+char words[] = "AaEeOoTtLl";
+char num[] = "4433007711";
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j <= 10; j++)
 		{
-		if (s[i] == word[j])
-		{
-			s[i] = num[j];
+			if (s[i] == words[j])
+			{
+				s[i] = num[j];
+			}
 		}
 	}
-	}
-	return (s);
+return (s);
 }
