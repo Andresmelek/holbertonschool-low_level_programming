edb43c60090302accc2cec50f247f285337e3d0b
Andresmelek <candres.isaza@gmail.com>
2019-10-03 22:37:32 -0500

    Fixing 6 - 7

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,17 +1,33 @@
 #include "holberton.h"
 /**
- * *cap_string - function that capitalizes all words of a string
- * s*: string
- * Return: returns the string
+ * cap_string - replaces a lowercase word in a string after signs or spaces
+ * @s: pointer to the string
+ * Return: the string
  */
-char *cap_string(char *)
+char *cap_string(char *s)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+int a;
+int flag;
+flag = 0;
+	for (a = 0; s[a] != '\0'; a++)
 	{
-	if (s[i] == ' ' || s[i] == ';' || s[i] == '.'
-	|| s[i] == '!' || s[i] == ';' || s[i] == '?' || s[i] == '"'
-	|| s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
-
+	if (s[a] == ',' || s[a] == ';' || s[a] == '.' || s[a] == '!' ||
+		s[a] == '?' || s[a] == '"' ||
+		s[a] == '(' || s[a] == ')' || s[a] == '{' || s[a] == '}' ||
+		s[a] == ' ' || s[a] == '\t' || s[a] == '\n')
+		{
+		flag = 1;
+		}
+	else if ((flag == 1 && s[a] >= 97 && s[a] <= 122) ||
+	(s[0] >= 97 && s[0] <= 122))
+		{
+		s[a] -= 32;
+		flag = 0;
+		}
+	else
+		{
+		flag = 0;
+		}
+	}
+return (s);
 }
