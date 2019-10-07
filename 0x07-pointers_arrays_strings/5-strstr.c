fa3810af588a8ac0689e1355a1c4c6251670b244
Andresmelek <candres.isaza@gmail.com>
2019-10-07 21:49:07 +0000

    Second try

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,10 +7,15 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack != *needle)
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		if (!*haystack++)
-		return (0);
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+				return (needle);
+		}
 	}
-	return (haystack);
+	return (0);
 }
