28745479a9ca35f0e7727568e463de7a35c0e22d
Andresmelek <candres.isaza@gmail.com>
2019-10-07 21:16:36 -0500

    Betty po favo

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,13 +1,10 @@
 #include "holberton.h"
-
 /**
  * set_string - use a pointer to derefrence another string
- * @s: pointer to pointer of string
+ * @s: double pointer
  * @to: pointer to string
- * Return: always success
  *
  */
-
 void set_string(char **s, char *to)
 {
 	*s = to;
