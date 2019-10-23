acdbec8bec23e1099e8cd582ac20a5a9e92877f1
Andresmelek <candres.isaza@gmail.com>
2019-10-23 18:03:09 -0500

    Fixing final

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,6 +10,7 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2, calc;
+	char *sign = argv[2];
 
 	if (argc != 4)
 	{
@@ -20,9 +21,9 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
-	if (argv[2] == '/' || argv[2] == '%' && num2 == 0)
+	if (((*sign == '/') || (*sign == '%')) && (num2 == 0))
 	{
-		printf("Error"\n);
+		printf("Error\n");
 		exit(100);
 	}
 	calc = (*get_op_func(argv[2]))(num1, num2);
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -49,7 +49,6 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-	if (b == 0)
 	{
 	return (a % b);
 	}
