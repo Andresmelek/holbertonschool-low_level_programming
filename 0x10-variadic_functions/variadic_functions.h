b1bec66628d254f43791c4727ff01dde696ef191
Andresmelek <candres.isaza@gmail.com>
2019-10-26 13:23:47 -0500

    Fixing library

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,6 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+#include <stdarg.h>
 /**
  * struct op - Options
  * @form: format
@@ -12,9 +9,12 @@ void print_all(const char * const format, ...);
  * Description: Functions
  */
 typedef struct op
-		{
-			char form;
-			void (*f)(va_list);
-		} op_t;
-
+{
+	char form;
+	void (*f)(va_list);
+} op_t;
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
