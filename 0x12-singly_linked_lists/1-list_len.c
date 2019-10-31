3a937da0fa06824b102b3ca0f2187dbf4fcb0e7f
Andresmelek <candres.isaza@gmail.com>
2019-10-31 12:19:10 -0500

    First approach with the linked lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * list_len - function that returns the number of elements
+ * @h: pointer to the first element
+ *  Return: the number of elements in a linked list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
