c3d3d3fc6984bbe045cb13845316e18d0524ebbd
Andresmelek <candres.isaza@gmail.com>
2019-11-05 09:00:34 -0500

    Fixing identation

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,8 +15,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *temp = *head, *new;
 
 	new = malloc(sizeof(listint_t));
-		if (new == NULL)
-			return (NULL);
+	if (new == NULL)
+		return (NULL);
 
 	new->n = n;
 	new->next = NULL;
