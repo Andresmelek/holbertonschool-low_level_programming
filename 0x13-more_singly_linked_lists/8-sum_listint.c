29ca6e679982063b704301b58f5f6ff0780bc0aa
Andresmelek <candres.isaza@gmail.com>
2019-11-05 09:56:05 -0500

    Sum of all the elements in the linked list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,8 +1,11 @@
-
-
-
-
-
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * sum_listint - function that returns the sum of all the data (n) of a list
+ * @head: pointer to the first node
+ * Return: the sum of all the data
+ */
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
