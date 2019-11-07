cb380c9335ce8b81b82e23e6ea8484d7585e913d
Andresmelek <candres.isaza@gmail.com>
2019-11-07 07:11:57 -0500

    First ones

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ *binary_to_uint - convert binary to decimal
+ *@b: pinter to binary number
+ *Return: decimal number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+	int  lenght = 0, power = 1, count = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[lenght] != '\0')
+	{
+		lenght++;
+	}
+	for (count = lenght - 1; count >= 0; count--, power *= 2)
+	{
+		if (b[count] & 1)
+		{
+			num +=  power;
+		}
+		if (b[count] != '0' && b[count] != '1')
+			return (0);
+	}
+return (num);
