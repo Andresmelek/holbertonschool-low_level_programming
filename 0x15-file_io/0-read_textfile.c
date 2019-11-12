6139273b4c91f40858a5c601c9b1085ca19a5766
Andresmelek <candres.isaza@gmail.com>
2019-11-12 12:24:16 -0500

    First approach with Files I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * read_textfile - function that reads a text file and prints it
+ * to the POSIX standard output.
+ * @filename: file that I want to read
+ * @letters: number of letters it should read and print
+ * Return: the actual number of letters it could read and print
+ * if the file can not be opened or read, return 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_descriptor, file_read, file_write;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+		return (0);
+
+	buf =  malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+
+	file_read = read(file_descriptor, buf, letters);
+	if (file_read == -1)
+		free(buf);
+		return (0);
+
+	file_write = write(STDOUT_FILENO, buf, file_read);
+	if (file_write == -1)
+		return (0);
+
+	close(file_descriptor);
+	free(buf);
+	return (file_write);
+}
