1996534cdfb01335f6674e0fba25d77d5617f974
Andresmelek <candres.isaza@gmail.com>
2019-12-15 15:59:26 -0500

    Print number of nodes

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,14 +1,16 @@
 #include "lists.h"
-
-
+/**
+ * print_distint - function that prints number of nodes
+ * @h: pointer to the first node
+ * Return: Number of nodes
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
 
 	if (h == NULL)
 		return (0);
-
-	while(h->next != NULL)
+	while (h->next != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
