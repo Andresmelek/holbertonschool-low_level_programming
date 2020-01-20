531dc5cb94b206be8645f35adaa5bfc699bbb8a2
Andresmelek <candres.isaza@gmail.com>
2020-01-20 00:14:20 -0500

    fixing output

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -18,9 +18,10 @@ void hash_table_delete(hash_table_t *ht)
 
 		while (tmp)
 		{
-			tmp1 = tmp->next;
+			tmp = tmp1;
 			free(tmp->key);
 			free(tmp->value);
+			tmp1 = tmp1->next;
 			free(tmp);
 			tmp = tmp1;
 		}
